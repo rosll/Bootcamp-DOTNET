08166be696dd77986d759ead0a0a6033e74c4d19
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-14 23:36:52 -0300

    inserção das migrations Produto e Categoria

--- 04-Desenvolvimento de aplicações com .NET/CursoMVC/Models/Context.cs
+++ 04-Desenvolvimento de aplicações com .NET/CursoMVC/Models/Context.cs
@@ -9,6 +9,8 @@ namespace CursoMVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
--- 04-Desenvolvimento de aplicações com .NET/CursoMVC/Models/Produto.cs
+++ 04-Desenvolvimento de aplicações com .NET/CursoMVC/Models/Produto.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace CursoMVC.Models
 {
-    public class Categoria
+    public class Produto
     {
         public int Id { get; set; }
         [Display(Name = "Descrição")]
         [Required(ErrorMessage = "O campo Descrição é obrigatório")]
+
         public string Descricao { get; set; }
-        public List<Produto> Produtos { get; set; }
+        [Required(ErrorMessage = "O campo Quantidade é obrigatório")]
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa")]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
